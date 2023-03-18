82f4157f51e81ad5aa0fc7d1b981080c03b084d2
An-nettt <113256885+An-nettt@users.noreply.github.com>
2023-03-18 06:55:43 +0000

    Deploying to gh-pages from @ An-nettt/goit-js-hw-09@75283fed5a10b23f2b93391e33ef64c0dd576f73 🚀

---
+++ 01-color-switcher.28ba0d63.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(function(t){console.log(t),Math.floor(16777215*Math.random()).toString(16).padStart(6,0)})),t.stopBtn.addEventListener("click",(function(t){}));
+//# sourceMappingURL=01-color-switcher.28ba0d63.js.map
---
+++ 01-color-switcher.b11169af.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(function(t){console.log(t),"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))})),t.stopBtn.addEventListener("click",(function(t){}))}();
+//# sourceMappingURL=01-color-switcher.b11169af.js.map
