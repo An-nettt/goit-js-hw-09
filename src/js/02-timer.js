0537c1c76320d31c6f260ff5cba0bd5597b34f09
Anna Kravets <annakravets1906@gmail.com>
2023-03-19 22:36:44 +0200

    Завдання 2. Вибір дати.

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,18 +1,68 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    // if (selectedDates) {
-    //   window.alert('Please choose a date in the future');
-    // }
-    console.log(selectedDates[0]);
-  },
+const refs = {
+  myInput: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
 };
 
-const myInput = document.querySelector('#datetime-picker');
-const fp = flatpickr(myInput, { options });
+refs.startBtn.disabled = true;
+
+const fp = flatpickr(
+  refs.myInput,
+  (options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    // isActive = false;
+    onClose(selectedDates) {
+      if (selectedDates[0] < new Date()) {
+        console.log('не то');
+        refs.startBtn.disabled = true;
+        // Notiflix.Notify.failure('Please choose a date in the future');
+        window.alert('Please choose a date in the future');
+      } else {
+        refs.startBtn.disabled = false;
+      }
+    },
+  })
+);
+
+// refs.startBtn.addEventListener('click', onStartBtnClick);
+
+// let timerInput = refs.myInput.value;
+
+// setTimeout(() => {
+//   localStorage.setItem('timerInput', refs.myInput.value);
+// }, 3000);
+
+// function onStartBtnClick(event) {
+//   console.log('click');
+
+//   console.log(localStorage.getItem('timerInput'));
+// }
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,24 @@
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', createPromise);
+
+let formData = {};
+
+refs.form.addEventListener('input', onInput);
+
+function onInput(event) {
+  formData[event.target.name] = event.target.value;
+
+  let delay = formData.delay;
+  return formData.step;
+  return formData.amount;
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    resolve('Success! Value passed to resolve function');
   } else {
-    // Reject
+    reject('Error! Error passed to reject function');
   }
 }
