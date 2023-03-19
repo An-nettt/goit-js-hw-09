46c6e06681438ea27304f4ca61257a8d5c9b3cf9
Anna Kravets <annakravets1906@gmail.com>
2023-03-19 23:39:24 +0200

    Завдання 2. Форматування часу

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,9 +5,19 @@ import Notiflix from 'notiflix';
 const refs = {
   myInput: document.querySelector('#datetime-picker'),
   startBtn: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
 };
 
+let today = new Date();
+let selectedDate;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+
 refs.startBtn.disabled = true;
+refs.days = 45;
 
 const fp = flatpickr(
   refs.myInput,
@@ -18,50 +28,56 @@ const fp = flatpickr(
     minuteIncrement: 1,
     // isActive = false;
     onClose(selectedDates) {
-      if (selectedDates[0] < new Date()) {
+      if (selectedDates[0] < today) {
         console.log('не то');
         refs.startBtn.disabled = true;
         // Notiflix.Notify.failure('Please choose a date in the future');
         window.alert('Please choose a date in the future');
       } else {
         refs.startBtn.disabled = false;
+        selectedDate = selectedDates[0];
       }
     },
   })
 );
 
-// refs.startBtn.addEventListener('click', onStartBtnClick);
+function onStartBtnClick(event) {
+  setInterval(() => {
+    let deltaTime = selectedDate.getTime() - Date.now();
+    // console.log(deltaTime);
+    let delta = convertMs(deltaTime);
+    console.log(delta);
+    refs.days = delta.days;
+    refs.hours = delta.hours;
+    refs.minutes = delta.minutes;
+    refs.seconds = delta.seconds;
+  }, 1000);
+}
 
-// let timerInput = refs.myInput.value;
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
 
-// setTimeout(() => {
-//   localStorage.setItem('timerInput', refs.myInput.value);
-// }, 3000);
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-// function onStartBtnClick(event) {
-//   console.log('click');
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
 
-//   console.log(localStorage.getItem('timerInput'));
-// }
-
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-
-//   // Remaining days
-//   const days = Math.floor(ms / day);
-//   // Remaining hours
-//   const hours = Math.floor((ms % day) / hour);
-//   // Remaining minutes
-//   const minutes = Math.floor(((ms % day) % hour) / minute);
-//   // Remaining seconds
-//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-//   return { days, hours, minutes, seconds };
-// }
+  return { days, hours, minutes, seconds };
+}
 
 // console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 // console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
