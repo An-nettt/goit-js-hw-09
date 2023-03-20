804e9838905c54c904a8796786e573247b69e9a1
An-nettt <113256885+An-nettt@users.noreply.github.com>
2023-03-20 17:17:42 +0000

    Deploying to gh-pages from @ An-nettt/goit-js-hw-09@7d08db60abd0c8b46845f17d047386d81f5367e2 🚀

--- 03-promises.699c3a06.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");e.addEventListener("submit",(function(e,t){Math.random()>.3?resolve("Success! Value passed to resolve function"):reject("Error! Error passed to reject function")}));var t={};e.addEventListener("input",(function(e){t[e.target.name]=e.target.value;t.delay;return t.step}))}();
-//# sourceMappingURL=03-promises.699c3a06.js.map
