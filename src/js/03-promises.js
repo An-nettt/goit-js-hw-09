52510b1336b6fb820e98888c815ad2d71dbf692c
Anna Kravets <annakravets1906@gmail.com>
2023-03-21 19:24:39 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,10 +2,6 @@ import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
 let formData = {};
-let delay = 0;
-let step = 0;
-let amount = 0;
-let position = 0;
 
 form.addEventListener('input', onInputClick);
 form.addEventListener('submit', onSubmitClick);
@@ -19,40 +15,44 @@ function onInputClick(event) {
   amount = Number(formData.amount);
 }
 
+function createPromise(position, nextDelay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, nextDelay);
+    console.log(nextDelay);
+  });
+}
+
 function onSubmitClick(event) {
   event.preventDefault();
   let nextDelay = delay;
 
   for (let index = 1; index <= amount; index += 1) {
     const position = index;
+    nextDelay += step;
+    console.log(nextDelay);
 
     createPromise(position, nextDelay)
-      .then((position, nextDelay) => {
+      .then(() => {
         Notiflix.Notify.success(
           `✅ Fulfilled promise ${position} in ${nextDelay}ms`
         );
+        console.log(nextDelay);
         // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
-      .catch((position, nextDelay) => {
+      .catch(() => {
         Notiflix.Notify.failure(
           `❌ Rejected promise ${position} in ${nextDelay}ms`
         );
         // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-    nextDelay += step;
-    console.log(nextDelay);
   }
 }
 
-function createPromise(position, nextDelay) {
-  return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-    setTimeout(() => {
-      if (shouldResolve) {
-        resolve(position, nextDelay);
-      } else {
-        reject(position, nextDelay);
-      }
-    }, delay);
-  });
-}
+console.log(54);
